91703142c4ba7ceeff67ed4e664aab3391d1ae72
Mithila Das <md3756@nyu.edu>
2020-07-30 17:50:02 -0400

    fixed player's token position

--- GameManager.py
+++ GameManager.py
@@ -5,12 +5,16 @@ import pygame, math
 class GameManager:
     def __init__(self):
         self.players = []
-        tokens = ['x', 'o']
+        # tokens = ['x', 'o']
+
+
+        tokens = ["./o.png", "./x.png"]
+
         self.num_players = 2 #int(input("Input number of players: ")) FIX THISSSSSSSSSSSSSSSSSS
         # Initialize all players with tokens
         for i in range(self.num_players):
-            token = tokens[i] #input("Input player " + str(i)+ "'s token: ") FIX THISSSSSSSSSSSSSSSSSS
-            self.players.append(p.Player(token))
+            #input("Input player " + str(i)+ "'s token: ") FIX THISSSSSSSSSSSSSSSSSS
+            self.players.append(p.Player(tokens[i]))
 
         # Index of player whose turn it is
         self.turn = 0
@@ -18,7 +22,7 @@ class GameManager:
         self.dimension = 3 #int(input("Input number of dimensions: ")) FIX THISSSSSSSSSSSSSSSSSS
 
         pygame.init()
-        self.font = pygame.font.SysFont('Arial', 20)
+        # self.font = pygame.font.SysFont('Arial', 40)
         self.width, self.height = 600, 600
         #2
         #initialize the screen
@@ -38,56 +42,37 @@ class GameManager:
             for offset_y, baby in enumerate(babylist):
                 baby.draw_board(offset_x, offset_y)
 
-
-    def convert(self, mouse_posn, divisor):
-        return [mouse_posn[1] // divisor, mouse_posn[0] // divisor]
-
-    def place_token(self, mama_coord, baby_coord):
-        # [0, 1] [1, 1] --> full_board: 900 perf_mouse_posn: [450, 150]
-        # [0, 1] [1, 2] --> [550, 150]
-        # [1, 1] [0, 1] --> [450, 350]
-        third = self.draw_length/ 3
-        x = mama_coord[1]*self.baby_length + baby_coord[1]*third + third/ 2 #- self.sub/3
-        y = mama_coord[0]*self.baby_length + baby_coord[0]*third + third/ 2 #- self.sub/3
-        print(x, y)
-
-        return (x,y)
-
     def update(self, cur_player):
         # Sleep to make the game 60 fps
         self.clock.tick(60)
+        row_height = self.game.length/self.dimension
 
         for event in pygame.event.get():
             if pygame.mouse.get_pressed()[0]:
                 #
-                mouse_posn = pygame.mouse.get_pos()
-                print(mouse_posn)
-                text = self.font.render(cur_player.token, False, (128,0,128))
-                # Convert mouse coordinate to equivalent mama board square
-                mama_coord = self.convert(mouse_posn, self.baby_length)
-
-                # Get new mouse position within baby board
-                x = mouse_posn[0] - mama_coord[1]*self.baby_length
-                y = mouse_posn[1] - mama_coord[0]*self.baby_length
-                new_mouse_posn = (x, y)
-                # Convert mouse position to identify location in baby board
-                baby_coord = self.convert(new_mouse_posn, math.floor(self.baby_length / 3))
+                self.mouse_posn = pygame.mouse.get_pos()
+                # text = self.font.render(cur_player.token, False, (128,0,128))
 
                 try:
+                    # Convert mouse coordinate to equivalent mama board square
+                    mama_coord, baby_coord = self.game.convert(self.mouse_posn)
+                    print("mama, baby",mama_coord, baby_coord)
+
                     # Given a location (mama_coord and baby_coord), try to place token
                     self.game.move(mama_coord[0], mama_coord[1], baby_coord[0], baby_coord[1], cur_player)
 
-                    location = self.place_token(mama_coord, baby_coord)
+                    location = self.game.place_token(self.mouse_posn)
 
-                    # location = mouse_posn # self.place_token(mama_coord, baby_coord)
-
-                    self.screen.blit(text, location)
+                    self.screen.blit(cur_player.token, location)
                     # Update player to next player since move was made successfully
                     self.turn = (self.turn + 1) % self.num_players
                     break
                 except ValueError:
                     # Placing token threw exception, move was invalid
                     print("try again friend")
+                except AssertionError:
+                    print("out of range")
+                    continue
 
 
             # Quit if the quit button was pressed
